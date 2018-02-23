c15c37e0bf3c90c63c49d9e50c5f1c50c8bfc365
Michael Bruno <michael.bruno1337@gmail.com>
2018-02-23 14:45:26 -0700

    Adjusting countdown layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {MapComponent} from './map/map.component';
+import { CountdownComponent } from './countdown/countdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    MapComponent
+    MapComponent,
+    CountdownComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/countdown/countdown.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-countdown',
+  templateUrl: './countdown.component.html',
+  styleUrls: ['./countdown.component.css']
+})
+export class CountdownComponent implements OnInit {
+
+  @Input() serverName: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
